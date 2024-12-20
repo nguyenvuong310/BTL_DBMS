126bcb2ae058bcb19af8d15498cb986fdef1702d
nguyenvuong310 <trungvuong2169@gmail.com>
2024-12-20 16:24:23 +0700

    add attribute appointment

--- backend/src/modules/doctor_schedules/doctor_schedules.controller.ts
+++ backend/src/modules/doctor_schedules/doctor_schedules.controller.ts
@@ -44,15 +44,15 @@ export class DoctorSchedulesController {
     return this.doctorSchedulesService.findDoctorScheduleByDoctorIdAndDay(id, normalizedDay);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDoctorScheduleDto: UpdateDoctorScheduleDto) {
-    return this.doctorSchedulesService.update(+id, updateDoctorScheduleDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateDoctorScheduleDto: UpdateDoctorScheduleDto) {
+  //   return this.doctorSchedulesService.update(+id, updateDoctorScheduleDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.doctorSchedulesService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.doctorSchedulesService.remove(+id);
+  // }
 
   @Get('/now/:doctorId')
   @Public()
